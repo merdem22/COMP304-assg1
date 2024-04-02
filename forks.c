1d105d179ba137262a38f41a0909744c2123b82f
merdem22 <merdem22@ku.edu.tr>
2024-04-02 13:12:06 +0000

    Added code that handles command line inputs as numbers, haven't started process creation yet.

--- forks.c
+++ forks.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {	
-	printf("Testing for git.");
+	
+	if (argc != 2)
+	{
+		printf("Please enter a single number as a command line argument to determine how many child processes will be created.");
+		return 1;
+	}
+
+
+	int x = atoi(argv[1]);
+
+	while (x > 0)
+	{
+	
+		printf("x: %d\n", x);
+		x--; 
+	}
+
 	return 0;
 }
