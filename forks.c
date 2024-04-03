e10d017bac4bd7e3ed9ec49541ff19390b0ac2f0
merdem22 <merdem22@ku.edu.tr>
2024-04-03 09:10:17 +0000

    added the fork function inside the while loop and get the pid for each process, added comments

--- forks.c
+++ forks.c
@@ -5,20 +5,26 @@
 int main(int argc, char *argv[])
 {	
 	
-	if (argc != 2)
+	
+
+		
+	if (argc != 2)//ensure command line arguments are entered properly.
 	{
 		printf("Please enter a single number as a command line argument to determine how many child processes will be created.");
 		return 1;
 	}
-
+	
 
 	int x = atoi(argv[1]);
+	int pid; //this variable will store the process id.
 
 	while (x > 0)
-	{
-	
-		printf("x: %d\n", x);
-		x--; 
+	{	
+		
+		fork();
+		pid = getpid();
+		printf("The process id is: %d\n", pid);
+		x--;
 	}
 
 	return 0;
