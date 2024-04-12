f48a06b627bc4c4f55ad540a52d085a7b24917ce
merdem22 <merdem22@ku.edu.tr>
2024-04-12 12:07:13 +0000

    Implemented the feature to read from input and assign to array, primitive version of checking x in each process, not functional yet.

--- search.c
+++ search.c
@@ -4,10 +4,19 @@
 #define MAX_NUMBERS 1000
 
 
-int main()
+int main(int argc, char* argv[])
 {
 	int numbers[MAX_NUMBERS];
 	int num_count = 0;
+
+
+	if (argc != 3)
+	{
+		//ensure propper command line arguments.
+		printf("Please enter x and n as command line arguments.");
+		return 1;
+	}
+
 
 	//assign numbers to array from stdin.
 	while (num_count < MAX_NUMBERS && scanf("%d", &numbers[num_count]) == 1)
@@ -16,11 +25,37 @@ int main()
 		while(getchar() != '\n');
 
 	}
-	//print the numbers.
-	for (int i = 0; i < MAX_NUMBERS; i++)
+	
+
+	int x = argv[1];
+	int processNum = argv[2];
+
+	int arraySize = MAX_NUMBERS/processNum;
+	
+	
+	for (int i = 0;	i < processNum; i++)
 	{
-		printf("%d", numbers[i]); //print the numbers for debugging.
-	}	
+		int val = fork();
+		if (val == 0)
+		{	
+			//search the array.
+			int startIndex = arraySize * i;
+			int endIndex = arraySize * (i+1);
+			for (int j = startIndex; j < endIndex; j++)
+			{
+				if (numbers[j] == x)
+				{
+					printf("X found.");
+					exit(0);
+				}
+			}
+			exit(1);
+		}
+		else
+		{	//parent waits for children.
+			wait();
+		}
+	}
 
 	
 	return 0;
