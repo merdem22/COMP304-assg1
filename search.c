3babb0f25016163ce1d45047048c5a7ac85ec6eb
merdem22 <merdem22@ku.edu.tr>
2024-04-15 14:14:56 +0000

    Finalized part 3, (search.c), added the feature to kill remaining the processes when a processes finds a match.

--- search.c
+++ search.c
@@ -3,14 +3,20 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+
 #define MAX_NUMBERS 1000
 
 
 int main(int argc, char* argv[])
 {
+	
+	//set up the arguments
+	int x = atoi(argv[1]);
+	int processCount = atoi(argv[2]);
+	pid_t childrenPIDS[processCount]; //get the pids from this array once its time to kill the children.
+
 	int numbers[MAX_NUMBERS];
 	int numCount = 0;
-
 
 	if (argc != 3)
 	{
@@ -18,7 +24,6 @@ int main(int argc, char* argv[])
 		printf("Please enter x and n as command line arguments.");
 		return 1;
 	}
-
 
 	//assign numbers to array from stdin.
 	while (numCount < MAX_NUMBERS && scanf("%d", &numbers[numCount]) == 1)
@@ -28,46 +33,56 @@ int main(int argc, char* argv[])
 
 	}
 	
-	int x = atoi(argv[1]);
-	int processCount = atoi(argv[2]);
 	int portionSize = numCount/processCount;
 	int remainder = numCount % processCount;
 	
 	for (int i = 0;	i < processCount; i++)
 	{
 		int val = fork();
+		childrenPIDS[i] = val;
 		if (val == 0)
 		{	
-			//search the array.
+			
 			int startIndex = portionSize * i;
 			int endIndex;
-			if (i == processCount - 1) //managing the windows borders.
+			if (i == processCount - 1) //setting up the borders of the array portion.
 			{
 				endIndex = numCount;
 			}
 			else
 			{	endIndex = startIndex + portionSize + (i < remainder ? 1 : 0); 
 			}
-			for (int j = startIndex; j < endIndex; j++)
+			for (int j = startIndex; j < endIndex; j++) //search the portion.
 			{
 				if (numbers[j] == x)
 				{
-					printf("X found.\n");
+					printf("Process with ID: %d has found X: %d at index: %d\n", getpid(), x, j);
 					exit(0);
 				}
 			
 			}
 			//not found
-			printf("Not found, for process %d\n", i+1);
+			printf("Process with ID: %d failed to find a match.\n", getpid());
 			exit(1);
 		}
+		else
+		{	int status;
+			waitpid(childrenPIDS[i], &status, 0); //wait for the current child to finish.
+			if (WIFEXITED(status) && WEXITSTATUS(status) == 0) //if the process exits with code 0.
+			{
+				for (int j = 0; j < i+1; j++)//kill the children processes.
+				{
+					if (j!=i)
+					{
+						kill(childrenPIDS[j], SIGKILL);
+						printf("Process with ID: %d is killed.\n", childrenPIDS[j]);
+					}
+				}
+				break;
+			}
+			
+		}
 	}
-
-	wait(NULL); //wait for children
-	
-	//kill(0, SIGKILL); //kill all remaining children. 
-
-
 	
 	return 0;
 }
